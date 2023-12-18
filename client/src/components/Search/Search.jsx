02690946e2d0defdda9358e78082e42c33472b19
Mihir Vir <80036612+MihirVir@users.noreply.github.com>
2023-12-18 20:02:50 +0000

    added search form

---
+++ client/src/components/Search/Search.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./search.css"
+const Search = () => {
+  return (
+    <div className = "airplane-container">
+        <div className = "image-container">
+            <img className = "plane-img" src="/airplane.png" alt="" />
+        </div>
+        <div className="search-bar-container">
+            <form className = "search-form">
+                <input className = "text-field" style ={{marginRight: "1.2%"}} type="text" placeholder='From'/>
+                <input className = "text-field" type="text" placeholder='To'/>
+                <button className='submit-btn'>Submit</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Search
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import "./home.css"
+import Search from '../../components/Search/Search'
 
 const Home = () => {
   return (
     <>
       <header className = "header">
-        <Header />
+          <Header />
+          <Search />
       </header>     
     </>
   )
